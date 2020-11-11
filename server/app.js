a985f345357c70a5aa4ccbadc5cee60caa4b592c
tracilevine <televine@gmail.com>
2020-11-11 15:25:08 -0500

    added open routes, controllers etc

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 require('./db/config');
 
 const express = require('express'),
-  app = express();
+  app = express(),
+  openRoutes = require('./routes/open/index');
 
 // Parse incoming JSON into objects
 app.use(express.json());
---
+++ server/routes/open/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router(),
+  { createUser, loginUser } = require("../../controllers/users");
+
+router.post("/", createUser);
+router.post("/login", loginUser);
+
+module.exports = router;
